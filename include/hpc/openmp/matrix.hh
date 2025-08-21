13d399c2f6e3bbfd81eb23f3c6e311bb984b24fb
dashygo097 <dashygo097@gmail.com>
2025-08-22 00:18:03 +0800

    update: vecadd benchmark and some optimizations

---
+++ include/hpc/openmp/matrix.hh
@@ -0,0 +1,11 @@
+#pragma once
+#define ENABLE_OPENMP
+
+namespace hpc::openmp {
+#ifdef ENABLE_OPENMP
+template <typename T> class Matrix {
+public:
+private:
+};
+#endif
+} // namespace hpc::openmp
---
+++ src/openmp/mmul_impl.cc
@@ -0,0 +1,6 @@
+#include "hpc/openmp/matrix.hh"
+
+namespace hpc::openmp {
+#ifdef ENABLE_OPENMP
+#endif
+} // namespace hpc::openmp
