fdf5c867b75802e8a104ff50079c708b5e2ed9ae
dashygo097 <dashygo097@gmail.com>
2025-09-02 16:43:58 +0800

    cmake env winok

--- include/hpc/cuda/cast.cuh
+++ include/hpc/cuda/cast.cuh
@@ -3,10 +3,10 @@
 #include <cuda_fp16.h>
 
 namespace hpc::cuda {
-#define INT2(value) *reinterpret_cast<int2 *>(&value)
-#define INT4(value) *reinterpret_cast<int4 *>(&value)
-#define FLOAT2(value) *reinterpret_cast<float2 *>(&value)
-#define FLOAT4(value) *reinterpret_cast<float4 *>(&value)
-#define HALF2(value) *reinterpret_cast<half2 *>(&value)
-#define BFLOAT2(value) *reinterpret_cast<__nv_bfloat162 *>(&value)
+#define INT2(value) (*reinterpret_cast<int2 *>(&value))
+#define INT4(value) (*reinterpret_cast<int4 *>(&value))
+#define FLOAT2(value) (*reinterpret_cast<float2 *>(&value))
+#define FLOAT4(value) (*reinterpret_cast<float4 *>(&value))
+#define HALF2(value) (*reinterpret_cast<half2 *>(&value))
+#define BFLOAT2(value) (*reinterpret_cast<__nv_bfloat162 *>(&value))
 } // namespace hpc::cuda
