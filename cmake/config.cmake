13858d91b8bb119867356f9158acb8f69f3c9d7a
dashygo097 <dashygo097@gmail.com>
2025-12-04 17:54:56 +0800

    reset: all impled cuda kernels

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,10 +1,11 @@
 # Build configuration options
 
-# Feature toggles
+# Backend toggles
 set(ENABLE_SIMD ON)
 set(ENABLE_OPENMP ON)
 set(ENABLE_MPI ON)
 set(ENABLE_CUDA OFF)
+set(ENABLE_ACCELERATE OFF)
 
 # Python options
 set(ENABLE_PYBIND11 OFF)
