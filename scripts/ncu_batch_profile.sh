fa378c58b5340c0043e6b1fcb1bf8d3960ee2de6
dashygo097 <dashygo097@gmail.com>
2025-12-04 17:56:20 +0800

    update: scripts/

--- scripts/ncu_batch_profile.sh
+++ scripts/ncu_batch_profile.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 CURRENT_DIR=$(pwd)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="$(dirname "$SCRIPT_DIR")"
-CUDA_EXEC_DIR="$BASE_DIR/build/outputs/cuda-demos"
+CUDA_EXEC_DIR="$BASE_DIR/bin/device"
 OUTPUT_DIR="$BASE_DIR/.profile"
 
 for exec_file in "$CUDA_EXEC_DIR"/*; do
