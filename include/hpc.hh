6fc31cedac4441c34e625a925ac8416329504fab
dashygo097 <dashygo097@gmail.com>
2025-12-07 00:24:25 +0800

    checkpoint

--- include/hpc.hh
+++ include/hpc.hh
@@ -3,14 +3,14 @@
 // Utils
 #include "./hpc/timer.hh"
 
+// Bindings
+#include "./hpc/bindings/bindings.hh"
+
 // Backends
 #include "./hpc/backends/backends.hh"
 
 // BLAS
 #include "./hpc/blas/blas.hh"
-
-// Bindings
-#include "./hpc/bindings/bindings.hh"
 
 // Neural Networks
 #include "./hpc/nn/nn.hh"
--- include/hpc/blas/l2/api.hh
+++ include/hpc/blas/l2/api.hh
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "../../backends/backends.hh"
+#include "./acceler.hh"
+#include "./cuda.cuh"
+#include "./omp.hh"
+#include "./omp_simd.hh"
+#include "./sequential.hh"
+#include "./simd.hh"
+#include <cstddef>
